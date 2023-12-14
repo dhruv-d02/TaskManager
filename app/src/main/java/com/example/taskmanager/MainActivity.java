6bbfdfded54c40df713a7636ed796ebd1e43ee75
dhruv-d02 <dhruvjain8522@gmail.com>
2023-12-15 02:19:35 +0530

    Basic Class Created

---
+++ app/src/main/java/com/example/taskmanager/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.taskmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView taskList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        taskList = findViewById(R.id.taskList);
+
+    }
+}
---
+++ app/src/main/java/com/example/taskmanager/TaskData.java
@@ -0,0 +1,32 @@
+package com.example.taskmanager;
+
+import java.util.Date;
+
+public class TaskData {
+    private String taskTitle, taskDescription;
+    private Date taskDate;
+
+    public String getTaskTitle() {
+        return taskTitle;
+    }
+
+    public void setTaskTitle(String taskTitle) {
+        this.taskTitle = taskTitle;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public Date getTaskDate() {
+        return taskDate;
+    }
+
+    public void setTaskDate(Date taskDate) {
+        this.taskDate = taskDate;
+    }
+}
