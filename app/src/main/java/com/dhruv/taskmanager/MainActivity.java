916d05a02c6cc70bf87daf96a16db2d7e166f53e
dhruv-d02 <dhruvjain8522@gmail.com>
2023-12-18 19:02:15 +0530

    On Button click sample data is adding.

--- app/src/main/java/com/dhruv/taskmanager/MainActivity.java
+++ app/src/main/java/com/dhruv/taskmanager/MainActivity.java
@@ -6,9 +6,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.dhruv.taskmanager.adapter.TaskAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -17,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     TaskAdapter taskAdapter;
     private ArrayList<TaskData> taskDataList = new ArrayList<>();
     private RecyclerView recyclerView;
+    private FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +37,20 @@ public class MainActivity extends AppCompatActivity {
         taskDataList.add(taskData4);
         taskDataList.add(taskData5);
         recyclerView = findViewById(R.id.recyclerView);
+        fab = findViewById(R.id.fab);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         taskAdapter = new TaskAdapter(this, taskDataList);
         recyclerView.setAdapter(taskAdapter);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TaskData taskData6 = new TaskData("Task6", "Task6");
+                taskDataList.add(taskData6);
+                taskAdapter = new TaskAdapter(getApplicationContext(), taskDataList);
+                recyclerView.setAdapter(taskAdapter);
+                Toast.makeText(getApplicationContext(), "Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
